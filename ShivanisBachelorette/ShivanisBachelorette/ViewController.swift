f432335150b6ea0d18fc524b3a1cf34ac89f5897
Vandita Sharma <vandita@getlatch.com>
2016-06-14 18:14:48 -0400

    project created

---
+++ ShivanisBachelorette/ShivanisBachelorette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShivanisBachelorette
+//
+//  Created by Vandita Sharma on 6/14/16.
+//  Copyright © 2016 Vandita Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
